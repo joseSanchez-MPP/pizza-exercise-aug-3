5bcd55ba7495eef332df8a7788af3df3159da59a
Jose Sanchez <josersanchez0117@gmail.com>
2020-08-03 13:23:51 -0500

    Initial Commit

---
+++ pizza/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pizza
+//
+//  Created by jose sanchez on 8/3/20.
+//  Copyright © 2020 jose sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
